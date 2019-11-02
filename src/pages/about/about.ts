7ef3a6a3ef46dd514107b4b939abb5f017b4e60a
Robert <robertojf2012@gmail.com>
2019-11-02 15:37:10 -0700

    coordenadas

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
 
 @Component({
   selector: 'page-about',
@@ -7,8 +8,34 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
+	latitud: number;
+	longitud: number;
 
+  constructor(public navCtrl: NavController, private geolocation: Geolocation) {
+  }
+
+  ionViewDidLoad() {
+    this.getCordenadas();
+  }
+
+  getCordenadas(){
+
+  	/*
+  	this.geolocation.getCurrentPosition().then((resp) => {
+			this.latitud = resp.coords.latitude; // resp.coords.latitude
+			this.longitud = resp.coords.longitude; // resp.coords.longitude
+		}).catch((error) => {
+		  console.log('Error getting location', error);
+		});
+		*/
+
+		let watch = this.geolocation.watchPosition();
+		watch.subscribe((data) => {
+		 // data can be a set of coordinates, or an error (if an error occurred).
+		 this.latitud = data.coords.latitude;
+		 this.longitud = data.coords.longitude;
+		 console.log(data);
+		});
   }
 
 }
